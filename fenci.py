40f034e9f2270ae43580d6ed0566e7e15022bf65
frankstar007 <568106896@qq.com>
2016-03-08 11:24:59 +0800

    添加停用词表

--- fenci.py
+++ fenci.py
@@ -10,14 +10,15 @@ def splitSentence(src,dst):
     fin = open(src,'r',encoding='utf-8')
     fout = open(dst,'a',encoding='utf-8')
 
-    fout.write(src)
+    stopwords = {}.fromkeys([line.rstrip() for line in open('stopkey.txt',encoding='utf-8')])
     for line in fin:
         contents = line.strip()
         wordlist = list(jieba.cut(contents))
         outstr = ''
-        for word in wordlist:
-            outstr += word
-            outstr += ' '
+        for words in wordlist:
+            if words not in stopwords:
+                outstr += words
+                outstr += ' '
         fout.write(outstr.strip())
     fout.write('\n')
     fin.close()
@@ -28,7 +29,7 @@ if __name__ == '__main__':
     path = os.getcwd()
     #导入配置文件
     conf = configparser.ConfigParser()
-    conf.read('setting.conf')
+    conf.read('setting.conf',encoding='utf-8')
 
     datafile = conf.get('datadir','rawdatadir')
     data     = conf.get('filepath','trainfile')
