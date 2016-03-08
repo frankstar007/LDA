c38405a2275b2675d1a7554b2a9c6181ffe7ad73
frankstar007 <568106896@qq.com>
2016-03-08 21:25:30 +0800

    预处理函数

--- lda.py
+++ lda.py
@@ -19,8 +19,35 @@ loggerInfo = logging.getLogger('TimeInfoLogger')
 Consolelogger = logging.getLogger('Consolelogger')
 #导入配置文件
 conf = configparser.ConfigParser()
-conf.read('setting.conf')
+conf.read('setting.conf',encoding='utf-8')
 #以列表的形式返回所有的section
 #sections = conf.sections()
 #options  = conf.options('filepath')
 #items  = conf.items('filepath')
+# 配置文件路径
+trainfile      = os.path.join(path,os.path.normpath(conf.get('filepath','trainfile')))
+wordidmapfile = os.path.join(path,os.path.normpath(conf.get('filepath','wordidmapfile')))
+thetafile      = os.path.join(path,os.path.normpath(conf.get('filepath','thetafile')))
+phifile        = os.path.join(path,os.path.normpath(conf.get('filepath','phifile')))
+paramfile      = os.path.join(path,os.path.normpath(conf.get('filepath','paramfile')))
+topNfile       = os.path.join(path,os.path.normpath(conf.get('filepath','topNfile')))
+tassginfile    = os.path.join(path,os.path.normpath(conf.get('filepath','tassginfile')))
+
+#模型初始参数
+K = int(conf.get('model_args','K'))
+alpha = float(conf.get('model_args','alpha'))
+beta = float(conf.get('model_args','beta'))
+iter_times = int(conf.get('model_args','iter_times'))
+top_words_num = int(conf.get('model_args','top_words_num'))
+
+#数据预处理
+def preprocessing():
+    Consolelogger.debug('载入数据......')
+    loggerInfo.debug('载入数据......')
+    with codecs.open(trainfile,'r',encoding='utf-8') as f:
+        docs = f.readlines()
+    Consolelogger.debug('载入完成，准备生成字典对象和统计文本数据...')
+    dpre = DataPrePreocessing()
+    items_idx = 0
+    for line in docs:
+        if line != '':
